d32c1ffee9cf66290469d7fefbbcdf98e6dc46a1
Changhua <lichanghua0821@gmail.com>
2022-08-20 13:48:52 +0800

    Reformat codes

--- Client/client.cpp
+++ Client/client.cpp
@@ -1,5 +1,5 @@
-﻿#include <stdio.h>
-#include <locale.h>
+﻿#include <locale.h>
+#include <stdio.h>
 
 #include "demo_h.h"
 #pragma comment(lib, "Rpcrt4.lib")
@@ -7,18 +7,15 @@
 BOOL WINAPI ctrlCHandler(DWORD /*signal*/)
 {
     unsigned long ulCode = 0;
-    RpcTryExcept
-    {
-        client_Shutdown();
-    }
-        RpcExcept(1)
+    RpcTryExcept { client_Shutdown(); }
+    RpcExcept(1)
     {
         ulCode = RpcExceptionCode();
         printf("ctrlCHandler: Runtime reported exception 0x%lX (%ld)\n", ulCode, ulCode);
     }
-    RpcEndExcept
+    RpcEndExcept;
 
-        exit(0);
+    exit(0);
 }
 
 int main()
@@ -26,19 +23,19 @@ int main()
     int ret = -1;
     RPC_STATUS status;
     RPC_WSTR pszStringBinding = NULL;
-    unsigned long ulCode = 0;
+    unsigned long ulCode      = 0;
 
-    setlocale(LC_ALL, "chs"); // 这是个大坑，不设置中文直接不见了。。。
+    setlocale(LC_ALL, "chs");                  // 这是个大坑，不设置中文直接不见了。。。
     SetConsoleCtrlHandler(ctrlCHandler, TRUE); // 注册 Ctrl+C 处理函数
     // Creates a string binding handle.
     // This function is nothing more than a printf.
     // Connection is not done here.
     status = RpcStringBindingCompose(NULL,                                             // UUID to bind to
-        reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"ncalrpc"), // Use TCP/IP protocol
-        NULL,                                             // TCP/IP network address to use
-        reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"tmp_endpoint"), // TCP/IP port to use
-        NULL,             // Protocol dependent network options to use
-        &pszStringBinding // String binding output
+                                     reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"ncalrpc"), // Use TCP/IP protocol
+                                     NULL,                                             // TCP/IP network address to use
+                                     reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"tmp_endpoint"), // TCP/IP port to use
+                                     NULL,             // Protocol dependent network options to use
+                                     &pszStringBinding // String binding output
     );
 
     if (status)
@@ -47,9 +44,9 @@ int main()
     // Validates the format of the string binding handle and converts
     // it to a binding handle.
     // Connection is not done here either.
-    status =
-        RpcBindingFromStringBinding(pszStringBinding, // The string binding to validate
-            &hDemoBinding);  // Put the result in the implicit binding(defined in the IDL file)
+    status
+        = RpcBindingFromStringBinding(pszStringBinding, // The string binding to validate
+                                      &hDemoBinding); // Put the result in the implicit binding(defined in the IDL file)
 
     if (status)
         exit(status);
@@ -59,23 +56,22 @@ int main()
         ret = client_SendString(L"Hello from 客户端");
         if (ret == 0) {
             wprintf(L"消息发送成功！\n");
-        }
-        else {
+        } else {
             wprintf(L"消息发送失败！\n");
         }
 
         // 发送完关闭通道
         client_Shutdown();
     }
-        RpcExcept(1)
+    RpcExcept(1)
     {
         ulCode = RpcExceptionCode();
         printf("main: Runtime reported exception 0x%lX (%ld)\n", ulCode, ulCode);
     }
-    RpcEndExcept
+    RpcEndExcept;
 
-        // Free the memory allocated by a string
-        status = RpcStringFree(&pszStringBinding);
+    // Free the memory allocated by a string
+    status = RpcStringFree(&pszStringBinding);
     if (status)
         exit(status);
 
--- Server/server.cpp
+++ Server/server.cpp
@@ -1,5 +1,5 @@
-﻿#include <stdio.h>
-#include <locale.h>
+﻿#include <locale.h>
+#include <stdio.h>
 
 #include "demo_h.h"
 #pragma comment(lib, "Rpcrt4.lib")
@@ -9,19 +9,17 @@ void server_Shutdown(void)
     RPC_STATUS status = RPC_S_OK;
 
     status = RpcMgmtStopServerListening(NULL);
-
     if (status) {
         exit(status);
     }
 
     status = RpcServerUnregisterIf(NULL, NULL, FALSE);
-
     if (status) {
         exit(status);
     }
 }
 
-int server_SendString(const wchar_t* msg)
+int server_SendString(const wchar_t *msg)
 {
     wprintf(L"服务器收到消息：%s\n", msg);
 
@@ -29,7 +27,7 @@ int server_SendString(const wchar_t* msg)
 }
 
 // Naive security callback.
-RPC_STATUS CALLBACK SecurityCallback(RPC_IF_HANDLE /*hInterface*/, void* /*pBindingHandle*/)
+RPC_STATUS CALLBACK SecurityCallback(RPC_IF_HANDLE /*hInterface*/, void * /*pBindingHandle*/)
 {
     return RPC_S_OK; // Always allow anyone.
 }
@@ -42,9 +40,9 @@ int main()
     // Uses the protocol combined with the endpoint for receiving
     // remote procedure calls.
     status = RpcServerUseProtseqEp(reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"ncalrpc"), // Use TCP/IP protocol
-        RPC_C_LISTEN_MAX_CALLS_DEFAULT,                   // Backlog queue length for TCP/IP.
-        reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"tmp_endpoint"), // TCP/IP port to use
-        NULL                                                   // No security
+                                   RPC_C_LISTEN_MAX_CALLS_DEFAULT,                   // Backlog queue length for TCP/IP.
+                                   reinterpret_cast<RPC_WSTR>((RPC_WSTR)L"tmp_endpoint"), // TCP/IP port to use
+                                   NULL                                                   // No security
     );
 
     if (status)
@@ -53,12 +51,12 @@ int main()
     // Registers the hello interface and auto listen
     // Equal to RpcServerRegisterIf + RpcServerListen
     status = RpcServerRegisterIf2(server_demo_v1_0_s_ifspec, // Interface to register.
-        NULL,                       // Use the MIDL generated entry-point vector.
-        NULL,                       // Use the MIDL generated entry-point vector.
-        RPC_IF_ALLOW_LOCAL_ONLY | RPC_IF_AUTOLISTEN, // Forces use of security callback.
-        RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Use default number of concurrent calls.
-        (unsigned)-1,                   // Infinite max size of incoming data blocks.
-        SecurityCallback);              // Naive security callback.
+                                  NULL,                      // Use the MIDL generated entry-point vector.
+                                  NULL,                      // Use the MIDL generated entry-point vector.
+                                  RPC_IF_ALLOW_LOCAL_ONLY | RPC_IF_AUTOLISTEN, // Forces use of security callback.
+                                  RPC_C_LISTEN_MAX_CALLS_DEFAULT, // Use default number of concurrent calls.
+                                  (unsigned)-1,                   // Infinite max size of incoming data blocks.
+                                  SecurityCallback);              // Naive security callback.
 
     while (1) {
         Sleep(10000); // 休眠，释放CPU
